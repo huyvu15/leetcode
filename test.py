614fd4c0ebc1887a6398cb615236d20a65cd489d
VuHuy <huyv80313@gmail.com>
2023-08-26 00:19:42 +0700

    add

--- test.py
+++ test.py
@@ -1,13 +1,41 @@
-# giải bài
+# # giải bài
+# import xml.etree.ElementTree as ET
 
-def word_cnt(s):
-    '''return strintg'''
+# def word_cnt(s):
+#     '''return strintg'''
+#     s = s.split(' ')
     
-
-
-
-s = "hello world hello"
-
-
+#     dicts = {}
+#     for ele in s:
+#         if ele in dicts:
+#             dicts[ele] += 1
+#         else:
+#             dicts[ele] = 1
+            
+#     print(dicts)
     
+#     root = ET.Element('words')
+#     for word, count in dicts.items():
+#         word_ele = ET.SubElement(root, "words")
+#         word_ele.text = word
+#         count_ele = ET.SubElement(root, "count")
+#         count_ele.text = str(count)
     
+#     tree = ET.ElementTree(root)
+#     output_file = "order.xml"
+#     tree.write(output_file, xml_declaration=True, encoding = 'utf-8', method='xml')
+
+# s = "hello world hello"
+# word_cnt(s)
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num ** (1/2) == num / (num**(0.5)):
+            return True
+        return False
+        
+a = Solution()
+print(a.isPerfectSquare(14))
