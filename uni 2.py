087e26133e214df48a29b116259096789e159e48
VuHuy <huyv80313@gmail.com>
2023-10-08 19:47:38 +0700

    add lih tinh

--- uni 2.py
+++ uni 2.py
@@ -1,35 +1,42 @@
-def uniquePathsWithObstacles(obstacleGrid):
-    if not obstacleGrid or not obstacleGrid[0]:
-        return 0
+def permute(nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        def backtrack(first):
+            if first == len(nums):
+                result.append(nums[::])
+            for i in range(first, len(nums)):
+                nums[first], nums[i] = nums[i], nums[first]
+                backtrack(first + 1) 
+                nums[first], nums[i] = nums[i], nums[first]
+        result = []
+        backtrack(0)
+        return result
+    
+def swap(a, b):
+    return b, a
 
-    m, n = len(obstacleGrid), len(obstacleGrid[0])
     
-    # Initialize a 2D DP array to store the number of unique paths
-    dp = [[0] * n for _ in range(m)]
+#code agian backtracking
+def func(nums):
+    def backtrack(start):
+        if start == len(nums):
+           results.append(nums[::])
+        for i in range(start, len(nums)):
+           swap(nums[start], nums[i])
+           backtrack(start + 1)
+           swap(nums[i]. nums[start])
+           
+           
+    results = []
     
-    # Initialize the top-left cell
-    dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+    backtrack(0)
+    return results
     
-    # Fill in the first row
-    for j in range(1, n):
-        if obstacleGrid[0][j] == 0:
-            dp[0][j] = dp[0][j - 1]
-    
-    # Fill in the first column
-    for i in range(1, m):
-        if obstacleGrid[i][0] == 0:
-            dp[i][0] = dp[i - 1][0]
-    
-    # Fill in the rest of the DP array
-    for i in range(1, m):
-        for j in range(1, n):
-            if obstacleGrid[i][j] == 0:
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-    
-    return dp[m - 1][n - 1]
 
-# Test cases
-obstacleGrid1 = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-obstacleGrid2 = [[0, 1], [0, 0]]
-print(uniquePathsWithObstacles(obstacleGrid1))  # Output: 2
-print(uniquePathsWithObstacles(obstacleGrid2))  # Output: 1
+
+nums1 = [1, 2, 3]   
+nums2 = [0,1]
+print(func(nums1))
+print(func(nums2))
